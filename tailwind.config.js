7ed30c053353fca454cee20703fc4723a8ddf73c
Spike Vinz <spikevinz@gmail.com>
2022-07-26 17:28:48 +0800

    replace js/css bundling with vite_rails
    fix tailwindcss
    add basic static home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
