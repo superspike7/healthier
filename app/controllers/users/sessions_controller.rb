67743da8ba56dda21ab7440241af3698588ec3f1
reyolan <reynardolan@gmail.com>
2022-07-27 02:46:53 +0800

    remove flash message when signing in and out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  
+  after_action :remove_notice, only: %i[destroy create]
+
+  private
+
+  def remove_notice
+    flash.delete(:notice)
+  end
 end
