acbffa7955b51ef561cd7f88ea00b0849a48201c
reyolan <reynardolan@gmail.com>
2022-07-27 11:52:09 +0800

    add user seed for easier testing of user features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(username: 'example123', email: 'example@example.com', password: 'abc123',
+             password_confirmation: 'abc123', confirmed_at: Time.now.utc)
