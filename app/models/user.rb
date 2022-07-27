a5e7513630c1ea12f796fb51584d9bd85b253796
reyolan <reynardolan@gmail.com>
2022-07-28 00:03:55 +0800

    add controller for conversation and members

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
+  has_many :conversation, through: :members
+  has_many :messages
+
   validates :username, format: { without: /\s/, message: 'Spaces are not allowed.' }, presence: true
 
   def self.from_omniauth(auth)
