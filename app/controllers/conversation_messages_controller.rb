9c7f5e0816867010999a1bc36ef07795ba863a4b
reyolan <reynardolan@gmail.com>
2022-07-28 07:49:33 +0800

    replace messages controller with conversation_messages controller

---
+++ app/controllers/conversation_messages_controller.rb
@@ -0,0 +1,6 @@
+class ConversationMessagesController < ApplicationController
+  def create
+    # pass the conversation id and user id
+    # create the message with the given params
+  end
+end
---
+++ app/helpers/conversation_messages_helper.rb
@@ -0,0 +1,2 @@
+module ConversationMessagesHelper
+end
