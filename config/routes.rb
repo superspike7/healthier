8794b412b79e1c5803a1049e42e23cdbb5811a96
reyolan <reynardolan@gmail.com>
2022-07-27 11:54:17 +0800

    remove unused google auth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_scope :user do
-    match '/auth/google/callback', to: 'users/omniauth_callbacks#google_oauth2',
-                                   via: %i[get post], as: 'user_google_oauth2_omniauth_callback'
+    get '/auth/google/callback', to: 'users/omniauth_callbacks#google_oauth2',
+                                 as: 'user_google_oauth2_omniauth_callback'
     get '/auth/failure', to: 'users/omniauth_callbacks#failure'
   end
 
