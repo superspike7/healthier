b95a53dafe838ec1be34df9321a796dcc3afac6c
reyolan <reynardolan@gmail.com>
2022-07-28 07:41:52 +0800

    add initial business logic for initiating of conversations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_034608) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_125625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_members_on_conversation_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "conversation_id", null: false
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,4 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_034608) do
     t.index ["username"], name: "index_users_on_username"
   end
 
+  add_foreign_key "members", "conversations"
+  add_foreign_key "members", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
