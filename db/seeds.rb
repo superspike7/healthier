158dfb18e05dc1f938b41d362b0e7b54ffe1a817
reyolan <reynardolan@gmail.com>
2022-07-28 00:06:06 +0800

    add more user seed to test chat feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!(username: 'example123', email: 'example@example.com', password: 'abc123',
-             password_confirmation: 'abc123', confirmed_at: Time.now.utc)
+5.times do |n|
+  User.create!(username: 'example123', email: "example#{n}@example.com", password: 'abc123',
+               password_confirmation: 'abc123', confirmed_at: Time.now.utc)
+end
