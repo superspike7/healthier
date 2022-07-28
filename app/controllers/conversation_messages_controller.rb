a902297d46cad1d8ef667405d1eda62bb271e6f4
reyolan <reynardolan@gmail.com>
2022-07-28 11:54:25 +0800

    fix logic for initiating of conversation and sending of message

--- app/controllers/conversation_messages_controller.rb
+++ app/controllers/conversation_messages_controller.rb
@@ -1,5 +1,11 @@
 class ConversationMessagesController < ApplicationController
   def create
-    current_user.messages.create(conversation_id: params[:conversation], content: params[:content])
+    current_user.messages.create(conversation_message_params)
+  end
+
+  private
+
+  def conversation_message_params
+    params.require(:message).permit(:content).merge(conversation_id: params[:conversation])
   end
 end
