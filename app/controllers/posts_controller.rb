e22fd20156a81504dd86f3e4fc96bbc1135ab164
ejamesssss <ejames.hegina@gmail.com>
2022-07-28 21:54:34 +0800

    remove bullet gem notifs for user,blob,attachment, but remove variant_records appear

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :update, :destroy]
   def index
-    @posts = Post.all
+    @posts = Post.all.with_attached_images.includes([:user])
   end
 
   def show
