64387cdb4a9e44a3367512af4c9f5f9c8523ead6
reyolan <reynardolan@gmail.com>
2022-07-29 18:51:08 +0800

    refactor logic for showing conversation name

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,7 @@
 class Member < ApplicationRecord
   belongs_to :conversation, counter_cache: true
   belongs_to :user
+
+  scope :other_username, ->(user) { where.not(user:).first_username }
+  scope :first_username, -> { first&.user&.username }
 end
