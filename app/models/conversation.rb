562ad6b6a712152da5992e1f1a3c2778145da2c2
reyolan <reynardolan@gmail.com>
2022-07-29 17:49:32 +0800

    add conversation name length validation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,8 @@
 class Conversation < ApplicationRecord
   has_many :members
 
+  validates :name, length: { maximum: 48 }
+
   def self.create_direct!(user, another_user)
     users = user == another_user ? { user: } : [{ user: }, { user: another_user }]
     ActiveRecord::Base.transaction do
