cae76c5cb11b55ca58009aee06242bd46c3448ea
reyolan <reynardolan@gmail.com>
2022-07-30 19:35:30 +0800

    finish member model test

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :member do
     association :user, factory: :user
     association :conversation, factory: :conversation
+
+    factory :other_member do
+      association :user, factory: :other_user
+    end
   end
 end
