4f40e27a5cb7f4754c06c38e9656c08584cc1421
EJamesssss <ejames.hegina@gmail.com>
2022-08-01 13:58:16 +0800

    change comments order to ascending

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-
+  scope :comment_desc, -> { order('created_at ASC') }
+  scope :include_user_only, -> { includes(:user) }
 end
