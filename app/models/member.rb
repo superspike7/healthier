584c3607618b91fb6f201911ebd7d4b6d513e69c
reyolan <reynardolan@gmail.com>
2022-08-02 10:43:36 +0800

    fix major message leak bug to other conversations

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,7 @@ class Member < ApplicationRecord
   belongs_to :user
 
   scope :other_username, ->(user) { where.not(user:).first_username }
+  scope :current_user_username, ->(user) { where(user:).first_username }
   scope :first_username, -> { first&.user&.username }
 
   # broadcasts_to ->(_convo_members) { [conversation, :members] }, inserts_by: :append, partial: 'direct_conversations/conversation',
