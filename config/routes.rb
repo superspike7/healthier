6e9a786b8182b58516d6f5f1718cb4c36f4b0608
Spike Vinz <spikevinz@gmail.com>
2022-08-03 08:18:03 +0800

    add custom route and controller for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   devise_scope :user do
     get '/auth/google/callback', to: 'users/omniauth_callbacks#google_oauth2',
                                  as: 'user_google_oauth2_omniauth_callback'
