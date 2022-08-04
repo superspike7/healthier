6c8a446f19f656a885bc68258f1ee8faeba1c978
reyolan <reynardolan@gmail.com>
2022-08-04 16:56:37 +0800

    add like notifications

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,15 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.new(like_params)
-    unless @like.save
-      flash[:notice] = @like.errors.full_messages.to_sentence
-    end
-
+    like = current_user.likes.create(like_params)
+    post = Post.find(like_params[:post_id])
+    LikeNotification.with(like:, post:, user: current_user).deliver_later(post.user)
     redirect_back_or_to root_path
   end
 
   def destroy
-    @like = current_user.likes.find(params[:id])
-    @like.destroy
+    like = current_user.likes.find(params[:id])
+    like.destroy
+    like.notifications_as_like.destroy_all
     redirect_back_or_to root_path
   end
 
---
+++ app/notifications/like_notification.rb
@@ -0,0 +1,30 @@
+# To deliver this notification:
+#
+# LikeNotification.with(post: @post).deliver_later(current_user)
+# LikeNotification.with(post: @post).deliver(current_user)
+
+class LikeNotification < Noticed::Base
+  deliver_by :database
+
+  param :post
+  param :user
+  param :like
+
+  def user_image
+    # whatever the user image implementation
+  end
+
+  def username
+    params[:user].username
+  end
+
+  def title
+    'commented on your post'
+  end
+
+  def body; end
+
+  def url
+    post_path(params[:post])
+  end
+end
