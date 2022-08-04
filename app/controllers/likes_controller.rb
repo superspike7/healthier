efee72d8832107b0c8c6b28518d5c99f45b5bb25
reyolan <reynardolan@gmail.com>
2022-08-04 14:10:25 +0800

    add realtime update for like count

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,22 @@
 class LikesController < ApplicationController
-    def create
-        @like = current_user.likes.new(like_params)
-        if !@like.save
-            flash[:notice] = @like.errors.full_messages.to_sentence
-        end
-
-        redirect_back_or_to root_path
+  def create
+    @like = current_user.likes.new(like_params)
+    unless @like.save
+      flash[:notice] = @like.errors.full_messages.to_sentence
     end
 
-    def destroy
-        @like = current_user.likes.find(params[:id])
-        post = @like.post
-        @like.destroy
-        redirect_back_or_to root_path
-    end
+    redirect_back_or_to root_path
+  end
 
-    private
+  def destroy
+    @like = current_user.likes.find(params[:id])
+    @like.destroy
+    redirect_back_or_to root_path
+  end
 
-    def like_params
-        params.require(:like).permit(:post_id)
-    end
+  private
+
+  def like_params
+    params.require(:like).permit(:post_id)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,21 @@
 class Like < ApplicationRecord
-
   validates :user_id, uniqueness: { scope: :post_id }
 
   belongs_to :user
   belongs_to :post
 
   scope :include_user_only, -> { includes(:user) }
+
+  after_create_commit do
+    broadcast_update_later_to 'likes',  partial: 'posts/like',
+                                        target: "post_likes_#{post.id}",
+                                        locals: { post:, user: Current.user }
+  end
+
+  after_destroy_commit do
+    broadcast_update_to 'likes', partial: 'posts/like',
+                                 target: "post_likes_#{post.id}",
+                                 locals: { post:, user: Current.user }
+  end
 end
+
