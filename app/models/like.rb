96af15be75067fa182f24406118984cc231aec52
reyolan <reynardolan@gmail.com>
2022-08-04 16:56:37 +0800

    add like notifications

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,15 +1,16 @@
 class Like < ApplicationRecord
-  validates :user_id, uniqueness: { scope: :post_id }
-
   belongs_to :user
   belongs_to :post
+  has_noticed_notifications model_name: 'Notification'
+
+  validates :user_id, uniqueness: { scope: :post_id }
 
   scope :include_user_only, -> { includes(:user) }
 
   after_create_commit do
-    broadcast_update_later_to 'likes',  partial: 'posts/like',
-                                        target: "post_likes_#{post.id}",
-                                        locals: { post:, user: Current.user }
+    broadcast_update_later_to 'likes', partial: 'posts/like',
+                                       target: "post_likes_#{post.id}",
+                                       locals: { post:, user: Current.user }
   end
 
   after_destroy_commit do
@@ -18,4 +19,3 @@ class Like < ApplicationRecord
                                  locals: { post:, user: Current.user }
   end
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_04_020538) do
   end
 
   create_table "conversations", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "members_count"
