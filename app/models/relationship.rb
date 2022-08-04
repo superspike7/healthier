977f0bec35a45ac25a0c0cba5292c700c8eef17e
EJamesssss <ejames.hegina@gmail.com>
2022-08-04 19:15:12 +0800

    add profile picture to users

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,5 @@
 class Relationship < ApplicationRecord
   validates :user_id, uniqueness: { scope: :followed_id }
   belongs_to :user
+  belongs_to :followed, class_name: "User", foreign_key: "followed_id"
 end
