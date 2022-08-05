1cdc23f98be21d551431cd2d6e6843710b0e123a
Spike Vinz <spikevinz@gmail.com>
2022-08-05 13:32:13 +0800

    add UI design for admin reports index page

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,4 +1,35 @@
 class Admin::ReportsController < AdminController
   def index
+    dummy_reports = [
+      { 
+        reporter: 'Spike',
+        type: 'user',
+        target: 'Vinz',
+        reasons: ['Sobrang Gwapo'],
+        description: 'A truncated Description...',
+        status: 'Pending',
+      },
+      { 
+        reporter: 'Raynard',
+        type: 'post',
+        target: 'How To Get 6packs in one Day',
+        reasons: ['Scam', 'Fake News'],
+        description: 'A truncated Description...',
+        status: 'Approved',
+      },
+      { 
+        reporter: 'Danica',
+        type: 'post',
+        target: 'Check out my New Exercise Course',
+        reasons: ['Advertisement', 'Promotion'],
+        description: 'A truncated Description...',
+        status: 'Rejected',
+      },
+    ]
+    @reports = dummy_reports 
   end
+
+  def show; end
+  def edit; end
+  def destroy; end
 end
