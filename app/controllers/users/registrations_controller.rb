9036ae15f9d226aaea6ae46892937b33651c4589
reyolan <reynardolan@gmail.com>
2022-08-05 08:35:19 +0800

    add restrictions in edit user page if google account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :restrict_google_user, only: :edit
+
+  private
+
+  def restrict_google_user
+    raise ActionController::RoutingError, 'Not Found' unless current_user.provider.nil?
+  end
 end
