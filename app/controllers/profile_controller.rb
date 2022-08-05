48fcec907b98c4b93312e37c4f3bf21fd9d31ed3
reyolan <reynardolan@gmail.com>
2022-08-05 08:28:51 +0800

    add profile controller, route, and views

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,20 @@
+class ProfileController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    current_user.update(profile_params)
+    redirect_to profile_url
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:username, :avatar)
+  end
+end
