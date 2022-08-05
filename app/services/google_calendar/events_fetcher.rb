8d772bc5807e453e6d1d9165c044092d8b999990
reyolan <reynardolan@gmail.com>
2022-08-05 17:24:49 +0800

    add initial logic for fetching all healthier events

--- app/services/google_calendar/events_fetcher.rb
+++ app/services/google_calendar/events_fetcher.rb
@@ -1,35 +1,22 @@
 
 module GoogleCalendar
-  class EventCreator < ApplicationService
+  class EventsFetcher < ApplicationService
     CALENDAR_NAME = 'Healthier'.freeze
 
-    def initialize(user, start, deadline, event_name)
+    def initialize(user)
       @user = user
-      @start = start # Time.current.to_datetime.rfc3339
-      @deadline = deadline #1.day.from_now.to_datetime.rfc3339
-      @event_name = event_name
     end
 
     def call
-      create_event
+      request_events
     end
 
     private
 
-    def create_event
+    def request_events
       authorize_google_client
       calendar_id = find_healthier_calendar_id
-      event = Google::Apis::CalendarV3::Event.new(
-        summary: @event_name,
-        start: Google::Apis::CalendarV3::EventDateTime.new(
-          date_time: @start,
-          time_zone: 'Manila'
-        ),
-        end: Google::Apis::CalendarV3::EventDateTime.new(
-          date_time: @deadline,
-          time_zone: 'Manila'
-      ))
-      @google_calendar_service.insert_event(calendar_id, event)
+      @google_calendar_service.list_events(calendar_id)
     rescue Google::Apis::AuthorizationError
       response = @oauth2_client.refresh!
       @user.update_access_token(response)
@@ -38,8 +25,8 @@ module GoogleCalendar
 
     def client_options
       {
-        access_token: User.first.access_token,
-        refresh_token: User.first.refresh_token,
+        access_token: @user.access_token,
+        refresh_token: @user.refresh_token,
         client_id: Figaro.env.google_client_id,
         client_secret: Figaro.env.google_client_secret,
         token_credential_uri: 'https://accounts.google.com/o/oauth2/token'
