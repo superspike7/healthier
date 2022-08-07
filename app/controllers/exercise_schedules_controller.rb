a2ec569e1918a55d3b58e375d9feef2f2828e144
reyolan <reynardolan@gmail.com>
2022-08-07 10:09:30 +0800

    add form for adding exercise schedule

--- app/controllers/exercise_schedules_controller.rb
+++ app/controllers/exercise_schedules_controller.rb
@@ -6,8 +6,17 @@ class ExerciseSchedulesController < ApplicationController
   def show; end
 
   def new
+    # list exercise categories here
   end
 
   def create
+    # API call
+    # GoogleCalendar::EventCreator.call
+  end
+
+  private
+
+  def exercise_schedule_params
+    params.require(:exercise_schedule).permit(:exercise_category, :start, :end)
   end
 end
