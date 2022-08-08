8153622c63adf4fccbc57e11fba827cf3635dbd1
reyolan <reynardolan@gmail.com>
2022-08-08 08:18:40 +0800

    rearrange method placement

--- app/services/google_calendar/event_creator.rb
+++ app/services/google_calendar/event_creator.rb
@@ -12,16 +12,6 @@ module GoogleCalendar
 
     private
 
-    def start_time
-      Time.new(@event_params['start(1i)'], @event_params['start(2i)'], @event_params['start(3i)'],
-               @event_params['start(4i)'], @event_params['start(5i)']).to_datetime.rfc3339
-    end
-
-    def end_time
-      Time.new(@event_params['end(1i)'], @event_params['end(2i)'], @event_params['end(3i)'],
-               @event_params['end(4i)'], @event_params['end(5i)']).to_datetime.rfc3339
-    end
-
     def create_event
       calendar_client = Clients::GoogleCalendar.call(@user)
       healthier_calendar_id = GoogleCalendar::CalendarIdFetcher.call(calendar_client, @user)
@@ -38,6 +28,16 @@ module GoogleCalendar
         start: Google::Apis::CalendarV3::EventDateTime.new(date_time: start_time, time_zone: 'Asia/Manila'),
         end: Google::Apis::CalendarV3::EventDateTime.new(date_time: end_time, time_zone: 'Asia/Manila')
       )
+    end
+
+    def start_time
+      Time.new(@event_params['start(1i)'], @event_params['start(2i)'], @event_params['start(3i)'],
+               @event_params['start(4i)'], @event_params['start(5i)']).to_datetime.rfc3339
+    end
+
+    def end_time
+      Time.new(@event_params['end(1i)'], @event_params['end(2i)'], @event_params['end(3i)'],
+               @event_params['end(4i)'], @event_params['end(5i)']).to_datetime.rfc3339
     end
   end
 end
