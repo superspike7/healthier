b09191311a8c465e4d021b709b7be430130d7fc8
reyolan <reynardolan@gmail.com>
2022-08-09 07:29:48 +0800

    fix errors in spec due to incorrect model names

--- app/mailers/userreport_mailer.rb
+++ app/mailers/userreport_mailer.rb
@@ -1,10 +1,4 @@
 class UserreportMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.userreport_mailer.violating_community_standard.subject
-  #
   def violating_community_standard
     @greeting = "Hi"
     @reported_user = params[:reported_user]
