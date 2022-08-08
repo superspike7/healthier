3d93679a1c3a2d8d31b78ed48e00e9091064eca0
reyolan <reynardolan@gmail.com>
2022-08-08 19:50:46 +0800

    fix receiver_id bug and error in rendering username and avatar

--- app/helpers/direct_conversations_helper.rb
+++ app/helpers/direct_conversations_helper.rb
@@ -1,2 +1,20 @@
 module DirectConversationsHelper
+
+  def conversation_username_to_display(members)
+    other = members.other_user(current_user)
+    if members.not_the_passed_user(current_user).blank?
+      current_user.username
+    else
+      other.username
+    end
+  end
+
+  def conversation_avatar_to_display(members)
+    other = members.other_user(current_user)
+    if members.not_the_passed_user(current_user).blank?
+      user_avatar(current_user, class: 'h-12 w-12 bg-green-200 rounded-full')
+    else
+      user_avatar(other, class: 'h-12 w-12 bg-green-200 rounded-full')
+    end
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -4,12 +4,21 @@ class Member < ApplicationRecord
 
   scope :other_username, ->(user) { where.not(user:).first_member&.username }
   scope :current_user_username, ->(user) { where(user:).first_member&.username }
-  scope :other_user, ->(user) { where.not(user:)&.first_member }
+  scope :other_user, ->(user) { not_the_passed_user(user)&.first_member }
   scope :current_user, ->(user) { where(user:)&.first_member }
+  scope :not_the_passed_user, ->(user) { where.not(user:) }
   scope :first_member, -> { first&.user }
 
   # broadcasts_to ->(_convo_members) { [conversation, :members] }, inserts_by: :append, partial: 'direct_conversations/conversation',
   #                                                                target: dom_id(converastion, :members),
   #                                                                locals: { member: self }
 
+  def self.message_username_to_display(user)
+    # query muna ng other = other_user
+    # check if user == current_user
+    # if user == current_user 
+    # other.username
+    # else
+    # current_user_username
+  end
 end
