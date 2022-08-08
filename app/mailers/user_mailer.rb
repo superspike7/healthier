04c2e058bef7bb8b55e7ca80071bd4c96b353fd4
reyolan <reynardolan@gmail.com>
2022-08-09 07:29:48 +0800

    fix errors in spec due to incorrect model names

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,4 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.update_username.subject
-  #
   def update_username
     @greeting = "Hi"
     @user = params[:user]
