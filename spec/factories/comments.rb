b8da6fb356a4dec05d403f0cc2817e53b54e632d
reyolan <reynardolan@gmail.com>
2022-08-09 09:26:22 +0800

    add comment model test

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :comment do
     association :user, factory: :user
     association :post, factory: :post
-    name { 'Category Test' }
-    description { 'Test Description' }
+    body { 'Test Comment' }
 
     factory :yesterday_comment do
       created_at { 1.day.ago }
