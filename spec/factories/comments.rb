9afea2011864d9aae9a339c36d65a20cbcec8a2e
reyolan <reynardolan@gmail.com>
2022-08-09 09:04:25 +0800

    install faker gem for testing

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     association :post, factory: :post
     name { 'Category Test' }
     description { 'Test Description' }
+
+    factory :yesterday_comment do
+      created_at { 1.day.ago }
+      updated_at { 1.day.ago }
+    end
   end
 end
