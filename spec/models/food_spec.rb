172e47a94ffc6cea7de0ae36f9cedb33cc8d636f
reyolan <reynardolan@gmail.com>
2022-08-09 10:58:24 +0800

    add food model test

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:food) { create(:food) }
+  let!(:other_food) { create(:food, name: 'Testfood') }
+
+  describe '.all_except' do
+    it 'returns all foods except the passed foods' do
+      expect(Food.all_except(other_food)).not_to include(other_food)
+    end
+  end
 end
