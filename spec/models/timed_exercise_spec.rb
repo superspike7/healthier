d6733f298fb49f29dbe6e7e947117eef7ad3eb01
reyolan <reynardolan@gmail.com>
2022-08-09 10:45:09 +0800

    add timed exercise model test

--- spec/models/timed_exercise_spec.rb
+++ spec/models/timed_exercise_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TimedExercise, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:timed_exercise) { create(:timed_exercise) }
+  let!(:other_timed_exercise) { create(:timed_exercise, name: 'Test2') }
+
+  describe '.all_except' do
+    it 'returns all timed exercises except the passed exercises' do
+      expect(TimedExercise.all_except(other_timed_exercise)).not_to include(other_timed_exercise)
+    end
+  end
 end
