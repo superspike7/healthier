7c5c671285e8711cb77415151b77e423b30b9736
reyolan <reynardolan@gmail.com>
2022-08-09 10:55:11 +0800

    add post model test

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :post do
     association :user, factory: :user
     body { 'Test body' }
+
+    factory :yesterday_post do
+      created_at { 1.day.ago }
+      updated_at { 1.day.ago }
+    end
   end
 end
