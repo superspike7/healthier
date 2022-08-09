6ba26663b3544b147b3d5ab51dc66ec4265e5e45
reyolan <reynardolan@gmail.com>
2022-08-09 11:36:42 +0800

    rewrite member model test

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -4,15 +4,39 @@ RSpec.describe Member, type: :model do
   let!(:first_member) { create(:member) }
   let!(:second_member) { create(:other_member, conversation: first_member.conversation) }
 
-  describe '.first_username' do
-    it 'returns the username of the first member in a member collection' do
-      expect(Member.first_member.username).to eq(first_member.user.username)
+  describe '.first_member' do
+    it 'returns a user record of the first member in a member collection' do
+      expect(Member.first_member).to eq(first_member.user)
+    end
+  end
+
+  describe '.not_the_passed_user' do
+    it 'returns a member collection that excludes the passed user' do
+      expect(Member.not_the_passed_user(first_member.user)).to_not include(first_member)
+    end
+  end
+
+  describe '.current_user' do
+    it 'returns the user record of the passed user' do
+      expect(Member.current_user(first_member.user)).to eq(first_member.user)
+    end
+  end
+
+  describe '.other_user' do
+    it 'returns the user record of the user other than the passed user' do
+      expect(Member.other_user(first_member.user)).to eq(second_member.user)
     end
   end
 
   describe '.other_username' do
-    it 'returns the username of the first user that is not the current user in a member collection' do
+    it 'returns the username of the user other than the passed user' do
       expect(Member.other_username(first_member.user)).to eq(second_member.user.username)
+    end
+  end
+
+  describe 'current_user_username' do
+    it 'returns the username of the passed user' do
+      expect(Member.current_user_username(first_member.user)).to eq(first_member.user.username)
     end
   end
 end
