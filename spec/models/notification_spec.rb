3d6d227e0b432e829b57321187a5f4774c1258dd
reyolan <reynardolan@gmail.com>
2022-08-09 11:19:47 +0800

    add notification model test

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:conversation) { create(:conversation) }
+  let(:message_notifications) { Notification.where(type: MessageNotification.name) }
+  let!(:message_notification) { MessageNotification.with(conversation:).deliver(user) }
+
+  describe '.all_except_message_notifications' do
+    it 'returns all notifications without message notifications' do
+      expect(Notification.all_except_message_notifications).to_not include(message_notification)
+    end
+  end
+
+  describe '.unread_messages' do
+    it 'returns unread message notifications' do
+      expect(Notification.unread_messages).to include(Notification.first)
+    end
+  end
 end
