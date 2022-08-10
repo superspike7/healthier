8845fa5b9feeeb1d0c8fdcc7c5324b6cc3ad8b35
reyolan <reynardolan@gmail.com>
2022-08-10 09:18:43 +0800

    render routine data in views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @rep_exercises = current_user.repetition_exercises
     @timed_exercises = current_user.timed_exercises
+    @routines = current_user.routines.include_associations
   end
 end
