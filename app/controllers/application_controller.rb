2a56e840e5151cf5785f55bc3fdf7ab5a277a65c
reyolan <reynardolan@gmail.com>
2022-08-10 11:47:56 +0800

    refactor edit profile logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,20 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,
     if: :devise_controller?
 
-      protected
+  protected
 
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
-      end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+  end
 
   private
 
   def set_current_user
     Current.user = current_user
+  end
+
+  def not_found
+    raise ActionController::RoutingError, 'Not Found'
   end
 end
