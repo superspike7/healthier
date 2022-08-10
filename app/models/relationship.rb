ca5b9999c914ca76980e6eb059e4c3f8affa77cf
reyolan <reynardolan@gmail.com>
2022-08-10 11:28:09 +0800

    add follower and following count

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,7 @@ class Relationship < ApplicationRecord
   validates :user_id, uniqueness: { scope: :followed_id }
   belongs_to :user
   belongs_to :followed, class_name: "User", foreign_key: "followed_id"
+
+  scope :following_count, ->(user) { where(user_id: user.id).count }
+  scope :follower_count, ->(user) { where(followed_id: user.id).count }
 end
