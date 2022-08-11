9b8faa72ea8d4848a1a308611e5a68f0a7c07265
Spike Vinz <spikevinz@gmail.com>
2022-08-12 05:40:22 +0800

    fix dyamic form for daily food intakes

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,15 +8,18 @@ class FoodsController < ApplicationController
   end
 
   def new
-    @new_food = current_user.foods.build
+    @new_food = current_user.foods.build(name: params[:name])
   end
 
   def create
     @new_food = current_user.foods.build(food_params)
-    if @new_food.save
-      redirect_to root_path, notice: "#{@new_food.name} has been successfully added."
-    else
-      render :new
+    respond_to do |format|
+      if @new_food.save
+        format.turbo_stream { flash.now[:notice] = "#{@new_food.name} has been successfully added." }
+        format.html { redirect_to root_path, notice: "#{@new_food.name} has been successfully added." } 
+      else
+        render :new
+      end
     end
   end
 
