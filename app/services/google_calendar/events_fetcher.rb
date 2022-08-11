f2646b48fe568319581a9b459632ef216916324b
reyolan <reynardolan@gmail.com>
2022-08-11 16:59:27 +0800

    remove unnecessary comments

--- app/services/google_calendar/events_fetcher.rb
+++ app/services/google_calendar/events_fetcher.rb
@@ -15,7 +15,6 @@ module GoogleCalendar
       calendar_client = Clients::GoogleCalendar.call(@user)
       healthier_calendar_id = GoogleCalendar::CalendarIdFetcher.call(calendar_client, @user)
       response = calendar_client.google_calendar_service.list_events(healthier_calendar_id)
-      # response.items.map { |e| Struct.new(:name, :start, :end).new(e.summary, e.start, e.end) }
       response.items.map { |e| { name: e.summary, start: e.start, end: e.end } }
     rescue Google::Apis::AuthorizationError
       response = calendar_client.oauth2_client.refresh!
--- spec/mailers/previews/userreport_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/userreport
-class UserreportPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/userreport/violating_community_standard
-  def violating_community_standard
-    UserreportMailer.violating_community_standard
-  end
-
-end
