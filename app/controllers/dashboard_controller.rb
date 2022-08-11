d364f91396016045bb43f6450f63cc3ecc4ae12f
reyolan <reynardolan@gmail.com>
2022-08-12 07:50:41 +0800

    remove unnecessary queries in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,7 @@ class DashboardController < ApplicationController
   before_action :authorize_user
 
   def index
-    @rep_exercises = current_user.repetition_exercises
     @timed_exercises = current_user.timed_exercises
-    @routines = current_user.routines.include_associations
     @daily_intake_today = current_user.daily_intakes.last
     @nutrients_description = Intake::NutrientsDescriptor.call(@daily_intake_today)
   end
