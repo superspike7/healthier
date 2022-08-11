3b52624ec751fd844618924a9f1df90ae0d5283b
reyolan <reynardolan@gmail.com>
2022-08-11 17:31:14 +0800

    edit devise parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
   def after_sign_in_path_for(_resource)
