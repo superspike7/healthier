a125d12775bfcec9e3888b29ad81e56763420bda
reyolan <reynardolan@gmail.com>
2022-08-11 12:17:02 +0800

    edit profile partial template for admin side

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,22 +1,20 @@
 class Admin::ReportsController < AdminController
-    before_action :set_report, only: [:show, :edit, :destroy]
+  before_action :set_report, only: [:show, :edit, :destroy]
 
-    def index
-        @reports = Report.includes([:reported]).order("created_at DESC")
-    end
+  def index
+    @reports = Report.includes([:reported]).order("created_at DESC")
+  end
 
-    def show
-        
-    end
+  def show; end
 
-    def destroy
-        @report.destroy
-        redirect_to admin_reports_path, notice: "Report deleted!"
-    end
+  def destroy
+    @report.destroy
+    redirect_to admin_reports_path, notice: "Report deleted!"
+  end
 
-    private
-    def set_report
-        @report = Report.find_by_id(params[:id])
-    end
+  private
 
+  def set_report
+    @report = Report.find_by_id(params[:id])
+  end
 end
