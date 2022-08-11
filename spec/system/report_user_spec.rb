bf11d09daa299b3fa8910143b4e8aaa5c720abef
Danica Limon <danica.limon1996@gmail.com>
2022-08-11 11:35:44 +0800

    add report a user system test

---
+++ spec/system/report_user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "user reports another user", type: :system do
+    let!(:user) { create(:user)}
+    let!(:another_user) { create(:user) }
+
+    it "submit report another user" do
+        sign_in user
+        visit profile_path(another_user.username)
+
+        click_link(href: "/profile/#{user.id}/report_user/new")
+
+        choose ("reason_suspicious_or_spam")
+        click_on "SUBMIT REPORT"
+
+        expect("Report User Successfully.").to be_present
+    end
+end
