9a4273f2492a76fb699e9d281f86d3fe01368863
reyolan <reynardolan@gmail.com>
2022-08-11 17:06:20 +0800

    change expectations in user report test

--- spec/system/report_user_spec.rb
+++ spec/system/report_user_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "user reports another user", type: :system do
-    let!(:user) { create(:user)}
-    let!(:another_user) { create(:user) }
+  let!(:user) { create(:user)}
+  let!(:another_user) { create(:user) }
 
-    it "submit report another user" do
-        sign_in user
-        visit profile_path(another_user.username)
+  it "submit report another user" do
+    sign_in user
+    visit profile_path(another_user.username)
 
-        click_link(href: "/profile/#{user.id}/report_user/new")
+    click_link(href: "/profile/#{user.id}/report_user/new")
 
-        choose ("reason_suspicious_or_spam")
-        click_on "SUBMIT REPORT"
+    choose ("reason_suspicious_or_spam")
+    click_on "SUBMIT REPORT"
 
-        expect("Report User Successfully.").to be_present
-    end
+    expect(page).to have_text("Report User Successfully.")
+  end
 end
