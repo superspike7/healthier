d524d77ec5240ddfbd57c9eb03b3aef12916188a
reyolan <reynardolan@gmail.com>
2022-08-12 09:23:27 +0800

    refactor like system test due to changes in view template

--- spec/system/like_post_spec.rb
+++ spec/system/like_post_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "like a post", type: :system do
-    let!(:user) { create(:user) }
-    let!(:post) {create(:post)}
+RSpec.describe "Liking a post", type: :system do
+  let!(:user) { create(:user) }
+  let!(:post) { create(:post) }
 
-    it 'change like button to unlike button once a post is liked' do
+  it 'increase like count of a post' do
+    new_like_count = 1
 
     sign_in user
-    visit posts_path
-    expect(page).to have_button('Like')
 
-    click_button('Like')
-    expect(page).to have_button('Unlike')
+    visit posts_path
+
+    within all('form > button')[0] do
+      find('svg').click
     end
+
+    expect(page).to have_text(new_like_count)
+  end
 end
